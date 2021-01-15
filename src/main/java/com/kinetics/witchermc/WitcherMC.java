4c34366557b33102c18807375bfc57074fc82a3b
Kine7ics <49065448+Kine7ics@users.noreply.github.com>
2021-01-15 23:09:39 +0100

    Sets up the base to work from.

---
+++ src/main/java/com/kinetics/witchermc/WitcherMC.java
@@ -0,0 +1,14 @@
+package com.kinetics.witchermc;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WitcherMC implements ModInitializer
+{
+    public static final String MOD_ID = "witchermc";
+
+    @Override
+    public void onInitialize()
+    {
+
+    }
+}
